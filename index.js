c2d738f2a6689f35c1eb076a0146789991797e19
tontonanrakyat <tontonanrakyat@gmail.com>
2025-07-03 02:27:34 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    var install1 = document.getElementById("install-1");
+    var install2 = document.getElementById("install-2");
+    var install3 = document.getElementById("install-3");
+
+    var setup1 = document.getElementById("setup-1");
+    var setup2 = document.getElementById("setup-2");
+    var setup2a = document.getElementById("setup-2a");
+    var setup2b = document.getElementById("setup-2b");
+    var setup3 = document.getElementById("setup-3");
+
+    var login1 = document.getElementById("login-1");
+    var login2 = document.getElementById("login-2");
+    var login3 = document.getElementById("login-3");
+
+    install1.innerHTML = strInstall1;
+    install2.innerHTML = strInstall2;
+    install3.innerHTML = strInstall3;
+
+    setup1.innerHTML = strSetup1;
+    setup2.innerHTML = strSetup2;
+    setup2a.innerHTML = strSetup2a;
+    setup2b.innerHTML = strSetup2b;
+    setup3.innerHTML = strSetup3;
+
+    login1.innerHTML = strLogin1;
+    login2.innerHTML = strLogin2;
+    login3.innerHTML = strLogin3;
+});
+
+var strInstall1 = "instal 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quam tortor, placerat eget lectus ac, iaculis aliquet ex. Vestibulum accumsan lorem sed aliquam vulputate. Nunc vitae sapien felis. In id nulla molestie urna scelerisque ultricies sed id lectus.";
+var strInstall2 = "instal 2 Fusce eleifend mi ac massa varius vestibulum. Etiam ultricies non turpis et pellentesque. Donec suscipit mi metus, id consectetur diam gravida id. Sed tincidunt ipsum eu mauris varius, non pellentesque erat vulputate. Ut et semper erat. Nulla mattis ut magna vel interdum.";
+var strInstall3 = "instal 3 Praesent at lorem nec sem porta facilisis ac non felis. Suspendisse id gravida nulla, id maximus neque. Mauris consectetur id diam facilisis consequat. Vestibulum non sem a urna elementum mollis nec at massa.";
+
+var strSetup1 = "setup 1 Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc scelerisque tellus et nisi vehicula dapibus.";
+var strSetup2 = "setup 2 Sed ac vestibulum neque, eu vulputate lorem. Phasellus sit amet odio dignissim, auctor mauris at, pharetra nibh. Nunc tristique placerat leo et bibendum. Aliquam at sagittis lorem, eget molestie erat. Vivamus quis magna erat.";
+var strSetup2a = "setup 2a Nunc gravida eros at lorem placerat, non ultrices arcu euismod. Pellentesque nisl nibh, suscipit sed quam a, hendrerit lacinia metus. Cras ac massa placerat, pretium elit a, sodales risus. Vivamus sollicitudin bibendum mattis.";
+var strSetup2b = "setup 2b Suspendisse auctor at metus ac euismod. Nunc vehicula, massa quis commodo hendrerit, augue mauris egestas urna, laoreet porta eros libero vel purus. Morbi magna risus, ultricies vel mattis in, vulputate sed neque.";
+var strSetup3 = "setup 3 Vestibulum odio dolor, rutrum id massa eget, aliquam varius ligula. Donec auctor scelerisque libero ac gravida. Nullam aliquam metus a vestibulum accumsan. Pellentesque imperdiet fringilla tortor ut posuere.";
+
+var strLogin1 = "login 1 Aenean risus massa, sagittis in condimentum sit amet, viverra et lacus. Fusce fringilla ex vitae erat vestibulum, vel scelerisque velit tristique. Sed nec velit eget mauris dictum hendrerit vel at ipsum. Morbi mattis purus vitae urna faucibus accumsan.";
+var strLogin2 = "login 2 Donec nisl erat, tincidunt vitae scelerisque ac, ultricies sit amet felis. Quisque hendrerit lorem ex, et pellentesque purus placerat vitae. Curabitur vel interdum nibh, sed posuere mi. Nunc auctor quam eu diam consequat, eu euismod neque tincidunt. Aliquam efficitur lorem odio, eget dictum arcu consequat vel. Interdum et malesuada fames ac ante ipsum primis in faucibus.";
+var strLogin3 = "login 3 Aenean risus massa, sagittis in condimentum sit amet, viverra et lacus. Fusce fringilla ex vitae erat vestibulum, vel scelerisque velit tristique. Sed nec velit eget mauris dictum hendrerit vel at ipsum. Morbi mattis purus vitae urna faucibus accumsan. Pellentesque sed elit rutrum, tincidunt purus at, rhoncus risus.";
+
+
